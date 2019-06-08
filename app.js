d43911cd7b49ef746fb9faf8689294554220ab37
searobea <searobea@gmail.com>
2019-06-08 15:02:18 -0500

    update

---
+++ app.js
@@ -0,0 +1,24 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper
+var svg = d3
+  .select("#heatmap")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+   
---
+++ config.js
@@ -0,0 +1,2 @@
+// Air Visual API Key:
+API_KEY= "KRnDumvqwn63BHNH3"
