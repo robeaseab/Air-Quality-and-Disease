f76cfcd59d7d64fdb23b6de209e0d1f0d596f897
searobea <searobea@gmail.com>
2019-06-12 11:40:06 -0500

    update

--- politicians.js
+++ politicians.js
@@ -9,22 +9,43 @@ function buildRepData(input) {
     var representativeUrl = "https://www.govtrack.us/api/v2/role?current=true&role_type=representative&limit=438"
 
     d3.json(senatorUrl).then(function (senatorData) {
-      console.log(senatorData);
+      var objects = senatorData.objects;
       
-    //   //     // Use d3 to select the panel with id of `#sample-metadata`
-    //   var panel = d3.select('#sample-metadata');
-    //   //     // Use `.html("") to clear any existing metadata
-    //   panel.html(" ");
+      for (var i = 0; i < objects.length; i++) {
+        
+        var state = objects[i].state;
+        var firstname = objects[i].person.firstname;
+        var lastname = objects[i].person.lastname;
+        var phone = objects[i].phone;
+        var title = objects[i].title_long;
+        var party = objects[i].party;
+        var address = objects[i].extra.address;
+        var contact = objects[i].extra.contact_form;
+        
+        console.log(state);
+      
+      
+
+
+
+      //     // Use d3 to select the panel with id of `#sample-metadata`
+      var panel = d3.select('#politicians-card');
+      //     // Use `.html("") to clear any existing metadata
+      panel.html(" ");
+      
   
-    //   panel.append('p').html(`AGE: ${metaData.AGE}`);
-    //   panel.append('p').html(`BBTYPE: ${metaData.BBTYPE}`);
-    //   panel.append('p').html(`ETHNICITY: ${metaData.ETHNICITY}`);
-    //   panel.append('p').html(`GENDER: ${metaData.Gender}`);
-    //   panel.append('p').html(`LOCATION: ${metaData.LOCATION}`);
-    //   panel.append('p').html(`WFREQ: ${metaData.WFREQ}`);
-    //   panel.append('p').html(`sample: ${sample}`);
+      panel.append('p').html(`STATE: ${state}`);
+      panel.append('p').html(`NAME: ${firstname} ${lastname}`);
+      panel.append('p').html(`TITLE: ${title}`);
+      panel.append('p').html(`PHONE: ${phone}`);
+      panel.append('p').html(`PARTY: ${party}`);
+      panel.append('p').html(`ADDRESS: ${address}`);
+      panel.append('p').html(`CONTACT:  <a href="${contact}">${firstname} ${lastname}</a> `);
   
+  };
+});
   
-    });
-  
-  }
+  };
+
+  buildRepData();
+  ;
