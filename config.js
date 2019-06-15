3f9ebc52fb652aa436e5e09e394c2e541e777778
searobea <searobea@gmail.com>
2019-06-15 14:09:14 -0500

    update

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
+
 // Air Visual API Key:
-API_KEY= "KRnDumvqwn63BHNH3"
+var API_KEY= "KRnDumvqwn63BHNH3";
+
+// Mapbox API key
+var MAPBOX_API_KEY = "pk.eyJ1Ijoic3JvYjU3NzAiLCJhIjoiY2p3Z2w1ZWt3MWxtMDQ0cHM5NzA2azVvYyJ9.YjL28BQkwrZEUve8Q-iAXQ";
