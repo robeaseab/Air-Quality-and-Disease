d8477054cd5d2d70dd91890dc2e7f448da379111
searobea <searobea@gmail.com>
2019-06-18 16:41:45 -0500

    update

--- heatmap.js
+++ heatmap.js
@@ -19,13 +19,13 @@ var svg = d3
   .attr("height", svgHeight);
 
 var myMap = L.map("map", {
-  center: [41.881832, -87.623177],
-  zoom: 2
+  center: [41.881832, -40.623177],
+  zoom: 6
 });
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-  maxZoom: 18,
+  maxZoom: 2,
   id: "mapbox.streets",
   accessToken: MAPBOX_API_KEY
 }).addTo(myMap);
