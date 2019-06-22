32a31b4a7ec993ed6418b4a31dadd13db886e424
searobea <searobea@gmail.com>
2019-06-22 14:11:46 -0500

    updated map

--- static/js/heatmap.js
+++ static/js/heatmap.js
@@ -20,12 +20,12 @@ var svg = d3
 
 var myMap = L.map("map", {
   center: [41.881832, -40.623177],
-  zoom: 6
+  zoom: 2
 });
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-  maxZoom: 2,
+  maxZoom: 20,
   id: "mapbox.streets",
   accessToken: MAPBOX_API_KEY
 }).addTo(myMap);
